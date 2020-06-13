d2fd759138d675a21dd6180bad978a7c890123b8
livinoid98 <ccy9803@naver.com>
2020-06-13 21:41:09 +0900

    Updates

---
+++ precache-manifest.b07191bfed39a38e7125121d2b238132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d8c168db57f2d443db6ce8a3766940",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "4a0e2ae335cc38ba90b6",
+    "url": "/react_movie_app/static/css/main.32208f45.chunk.css"
+  },
+  {
+    "revision": "d17a0903073d4ea07fc9",
+    "url": "/react_movie_app/static/js/2.d5845d61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.d5845d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0e2ae335cc38ba90b6",
+    "url": "/react_movie_app/static/js/main.1ba16481.chunk.js"
+  },
+  {
+    "revision": "accda7a530ed454f8288",
+    "url": "/react_movie_app/static/js/runtime-main.1230773c.js"
+  }
+]);
